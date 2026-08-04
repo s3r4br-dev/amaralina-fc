55dc2748f5e0d61b6df0dd1e489838cc7e1064a5
Gustavo Meireles <s3r4br@gmail.com>
2026-08-04 15:01:24 -0300

    Updated next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -3,8 +3,24 @@ const nextConfig = {
   typescript: {
     ignoreBuildErrors: true,
   },
+
   images: {
     unoptimized: true,
+  },
+
+  webpack: (config) => {
+    config.parallelism = 1
+
+    config.cache = false
+
+    config.resolve.symlinks = false
+
+    config.snapshot = {
+      managedPaths: [],
+      immutablePaths: [],
+    }
+
+    return config
   },
 }
 
